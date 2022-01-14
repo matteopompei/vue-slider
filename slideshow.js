dafa80775626c44165ace1e7dfb6ec4007a8ff1d
Matteo Pompei <datxizzan@gmail.com>
2022-01-14 16:12:06 +0100

    Array di oggetti creato

--- slideshow.js
+++ slideshow.js
@@ -1,27 +1,36 @@
-// Definiamo gli Array
-const imgArr = [
-  'img/01.jpg',
-  'img/02.jpg',
-  'img/03.jpg',
-  'img/04.jpg',
-  'img/05.jpg'
-];
-
-const titleArr = [
-  'Svezia',
-  'Svizzera',
-  'Gran Bretagna',
-  'Germania',
-  'Paradise'
-];
-
-const textArr = [
-  'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-  'Lorem ipsum',
-  'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-  'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-  'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-];
+let app = new Vue ({
+  el: "#container",
+  data: {
+    corrente: 0,
+    slideArr: [
+      {
+        image: "img/01.jpg",
+        title: "Svezia",
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis."
+      },
+      {
+        image: "img/02.jpg",
+        title: "Svizzera",
+        text: "Lorem ipsum"
+      },
+      {
+        image: "img/03.jpg",
+        title: "Gran Bretagna",
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
+      },
+      {
+        image: "img/04.jpg",
+        title: "Germania",
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,"
+      },
+      {
+        image: "img/05.jpg",
+        title: "Paradise",
+        text: "Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,"
+      }
+    ]
+  }
+});
 
 // Definiamo variabili
 let mainImage = document.getElementById("slideshow");
